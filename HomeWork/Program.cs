bacc73db0c612a1c3d53b4859155a48df6e97761
Sergej Mikhasko <mikhassergej@yandex.com>
2020-01-14 12:37:25 +0300

    Создание проекта

---
+++ HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MkGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Work :(");
+        }
+    }
+}
